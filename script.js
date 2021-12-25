003a45e152f3531643ee4f60c26088d40d07385b
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-12-25 12:51:37 -0400

    Corrigindo erros, implementando botões anterior e próximo

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-let musica = document.querySelector('audio');
-let duracao = document.querySelector('.fim');
+let musicas = [
+   {titulo: 'Classical Rock', 
+    artista: 'Deyvson Aguiar', 
+    src: 'musicas/Patterns Of Sadness - Jeremy Korpas.mp3',
+    img: 'imagens/rock_music.jpg'
+    },
+    {titulo: 'Pop Music', 
+    artista: 'Deyvson Aguiar', 
+    src: 'musicas/Crazy - Patrick Patrikios.mp3',
+    img: 'imagens/pop_music.jpg'
+    },
+    {titulo: 'Classical Music', 
+    artista: 'Deyvson Aguiar', 
+    src: 'musicas/Mi Dispiace - Mini Vandals.mp3',
+    img: 'imagens/classical_music.jpg'
+    }
+];
 
-duracao.textContent = segundosParaMinutos(Math.floor(musica.duration));
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+let duracao = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
 
 //eventos
 document.querySelector('.play').addEventListener('click', tocarMusica);
 document.querySelector('.pause').addEventListener('click', pausarMusica);
 musica.addEventListener('timeupdate', atualizarBarra);
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if(indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+document.querySelector('.proximo').addEventListener('click', () => {
+    indexMusica++;
+    if(indexMusica > 2) {
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+
+});
 
 //funções
+function renderizarMusica(index) {
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracao.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
 function tocarMusica() {
     musica.play();
     document.querySelector('.pause').style.display = 'block';
@@ -37,3 +84,5 @@ function segundosParaMinutos(segundos) {
 
     return campoMinutos+ ':' + campoSegundos;
 }
+
+
